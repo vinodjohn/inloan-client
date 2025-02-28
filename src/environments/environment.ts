f1605772803dfed577a722647e5cd1a9c8c85424
vinodjohn <vinodjohn56@gmail.com>
2025-02-28 13:55:06 +0200

    Updated AWS URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   development: false,
   environmentName: 'PROD',
-  baseURL: 'http://54.93.218.156:8080/',
+  baseURL: 'http://3.64.11.255:8080/',
   itemsPerPage: 10
 };
